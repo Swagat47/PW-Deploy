9ee190325d0a44ac33bb100933e02fcbebc0cf86
Swagat47 <swagatraj47@gmail.com>
2023-03-13 14:38:43 +0530

    fix paths

--- server/index.ts
+++ server/index.ts
@@ -15,7 +15,7 @@ const app = express();
 //console.log("-------------------------------------------------------");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({ origin: "*", credentials: true }));
 
 app.use(morgan("dev"));
 app.use(cookieParser());
